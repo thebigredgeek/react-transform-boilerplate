15df322ae4515f4571c3767ad5937ea20af63e5d
The Big Red Geek <rhyneandrew@gmail.com>
2016-01-20 23:11:59 -0800

    added todomvc for redux boilerplate

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,5 +1,5 @@
-var path = require('path');
-var webpack = require('webpack');
+var path = require('path')
+  , webpack = require('webpack');
 
 module.exports = {
   devtool: 'cheap-module-eval-source-map',
@@ -22,6 +22,10 @@ module.exports = {
       test: /\.jsx?/,
       loaders: ['babel'],
       include: path.join(__dirname, 'src')
+    }, {
+      test: /\.css?$/,
+      loaders: [ 'style', 'raw' ],
+      include: __dirname
     }]
   }
 };
